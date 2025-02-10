9e433132a8a2f0d75e3d3f93bba8fcfdec4505f3
ChloeeeYZ <162940591+ChloeeeYZ@users.noreply.github.com>
2025-02-10 03:29:24 -0500

    Update game_top.v

--- game_top.v
+++ game_top.v
@@ -40,25 +40,25 @@ wire done;
 
 
 
-//assign reset = ~KEY[0];
+assign reset = ~KEY[0];
 
-//clock_sec c0(CLOCK_50, reset, clk);
+clock_sec c0(CLOCK_50, reset, clk);
 
 
 
-//ps2_top p0(
-//
-//	CLOCK_50,
-//	KEY[0],
-//
-//	PS2_CLK,
-//	PS2_DAT,
-//	
-//	rotate,
-//	left,
-//	right
-//
-//);
+ps2_top p0(
+
+	CLOCK_50,
+	KEY[0],
+
+	PS2_CLK,
+	PS2_DAT,
+	
+	rotate,
+	left,
+	right
+
+);
 
 
 datapath n0(
@@ -108,51 +108,3 @@ time_up
 
 endmodule
 
-
-
-reg enable;
-
-
-always@(negedge clk)
-begin
-	if(reset)
-	begin
-		enable <= 0;
-		counter <= 0;
-		end
-	else if (start)
-	begin
-		enable <= 0;
-		counter <= 0;
-	end
-	else if(curr_state == S_WAIT)
-	begin
-		if (counter == 2'd3)
-		begin
-			counter <= 0;
-			enable <= 1;
-			end
-		else
-		begin
-		counter <= counter + 1;
-		enable <= 0;
-		end
-	end
-	else
-	begin
-		counter <= counter;
-		enable <= enable;
-	end
-end		
-
-always @(posedge clk or posedge reset)
-begin
-	if (reset)
-		time_up <= 4'b0000;
-	else if (start)
-		time_up <= 4'b0011;
-	else if (enable)
-		time_up <= time_up-4'b0001;
-	else
-		time_up <= time_up;
-end
